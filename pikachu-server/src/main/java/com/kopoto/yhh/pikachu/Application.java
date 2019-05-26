9ffcb9dec0139b2c7bbc1b44817607957e8f27cb
yhh <827732439@qq.com>
2019-05-26 19:35:18 +0800

    fix ...

--- pikachu-server/src/main/java/com/kopoto/yhh/pikachu/Application.java
+++ pikachu-server/src/main/java/com/kopoto/yhh/pikachu/Application.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
