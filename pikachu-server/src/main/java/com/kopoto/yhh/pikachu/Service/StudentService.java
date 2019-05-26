70e44eee9ae66e573e833c2d3690b08e08a6f84c
yhh <827732439@qq.com>
2019-05-26 19:10:20 +0800

    fix ...

--- pikachu-server/src/main/java/com/kopoto/yhh/pikachu/Service/StudentService.java
+++ pikachu-server/src/main/java/com/kopoto/yhh/pikachu/Service/StudentService.java
@@ -2,10 +2,12 @@ package com.kopoto.yhh.pikachu.Service;
 
 import com.kopoto.yhh.pikachu.dao.StudentDao;
 import com.kopoto.yhh.pikachu.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+    @Autowired
     private StudentDao studentDao;
 
     public Student one(int id) {
